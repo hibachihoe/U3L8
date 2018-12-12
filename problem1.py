22e59500d333b798c500d8ac98eeb3a2efa4193b
hibachihoe <43859732+hibachihoe@users.noreply.github.com>
2018-12-12 12:41:59 -0500

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,15 @@
+print('-'*80)
+print('Morbid Predictions Program:')
+print('Description: This program asks you for your current age and tells you the year that you will die (on average).')
+print()
+age= input('What is your age today? ')
+age= int(age)
+print('...thinking...')
+print('...thinking...')
+print('...thinking...')
+print('You will die in the year...')
+birth= 2018 - age
+birth= int(birth)
+answer= birth + 79
+answer= int(answer)
+print(answer)
